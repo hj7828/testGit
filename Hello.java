b8a82a584cc92be9930c7699869b92c3bdf41a08
9527828 <9527828@naver.com>
2017-04-18 14:10:44 +0900

    B commit

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
   public static void main(String[] arge) {
     System.out.println("Hello, world from GIT");
     System.out.println("devel commit");
+    System.out.println("B commit");
   }
 }
